82d46d126f9221cff8f0914f58f86596adafdfc3
mohitsh7979 <mohitsh7979@gmail.com>
2024-03-24 00:29:38 +0530

    update

---
+++ React_router_project/src/App.jsx
@@ -0,0 +1,19 @@
+import About from './About'
+import './App.css'
+import Header from './Header'
+import { Outlet } from 'react-router-dom'
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <Header />
+    <Outlet />
+    
+    </>
+  )
+}
+
+export default App
