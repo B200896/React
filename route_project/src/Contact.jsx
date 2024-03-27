152de3c819fb78fb1f0c1234fb384d7ef0c6a1b8
mohitsh7979 <mohitsh7979@gmail.com>
2024-03-27 23:51:23 +0530

    update

--- route_project/src/Contact.jsx
+++
@@ -1,9 +0,0 @@
-function Contact(){
-   
-    return(
-
-        <h1>This is my Contact page</h1>
-    )
-}
-
-export default Contact;
--- route_project/src/Product.jsx
+++
@@ -1,9 +0,0 @@
-function Product(){
-
-     return(
-
-        <h1>This is my product page</h1>
-     )
-}
-
-export default Product;
