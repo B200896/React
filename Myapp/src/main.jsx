085fcb5f1ce51d485346963eaae2cb062c0505aa
kashish <kashishkaramchandani3@gmail.com>
2024-04-03 19:32:41 +0530

    added

--- Myapp/src/main.jsx
+++ Myapp/src/main.jsx
@@ -8,6 +8,7 @@ import { Route} from 'react-router-dom'
 import Home from './Home/Home.jsx'
 import Product from './Our/Product.jsx'
 import About from './About.jsx'
+import Contact from './Contact.jsx'
 
 // import './index.css'
 
@@ -19,6 +20,7 @@ const router = createBrowserRouter(
         <Route path='' element = {<Home />}/>
         <Route path='our_product' element = {<Product />} />
         <Route path='about' element = {<About />} />
+        <Route path='contact' element ={<Contact/>}/>
      </Route>
    )
 )
