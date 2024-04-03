4af423108657d1ba6539c98c23311d7fffa7d8b4
mohitsh7979 <mohitsh7979@gmail.com>
2024-04-04 01:05:17 +0530

    Update

--- Myapp/src/About/About.jsx
+++ Myapp/src/About/About.jsx
@@ -1,10 +1,142 @@
-function About(){
-   
-    return(
+import './About.css'
+function About() {
+    return (
+        <div className="section">
+            <div className="image">
+                <img src="https://templatemo.com/templates/templatemo_546_sixteen_clothing/assets/images/about-heading.jpg"
+                    alt="" />
+            </div>
+            <div className="section-1">
+                <h4>About us</h4>
+                <h2>Our Company</h2>
+                <img src="https://templatemo.com/templates/templatemo_546_sixteen_clothing/assets/images/about-heading.jpg" alt="" />
+            </div>
+            <div className="background">
+                <h2>Our Background</h2>
+            </div>
+            <hr />
+            <div className="further">
+                <div className="right">
+                    <img src="https://templatemo.com/templates/templatemo_546_sixteen_clothing/assets/images/feature-image.jpg"
+                        alt="" />
+                </div>
+                <div className="left">
+                    <h4>Who we are & what we do?</h4>
+                    <p>It is very important for the customer to pay attention to the adipiscing process. But with pleasure
+                        nothing follows him and the like? It follows that, if they happen, he will only criticize the pains
+                        of the chosen one, and he will not flee from the said pain by flattery. It is very important for the
+                        customer to pay attention to the adipiscing process.</p>
+                    <hr />
+                </div>
+            </div>
+            <div className="team">
+                <h2>Our Team Members</h2>
+                <hr />
+            </div>
+            <div className="pictures">
+                <div className="image">
+                    <img src="https://templatemo.com/templates/templatemo_546_sixteen_clothing/assets/images/team_01.jpg"
+                        alt="" />
+                    <h4>Johnny William</h4>
+                    <span>Co-Founder</span>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid aliquam error beatae tenetur eum
+                        maiores reprehenderit tempore quod sed dolorum.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione consequatur autem voluptas corporis</p>
+                </div>
+                <div className="image">
+                    <img src="https://templatemo.com/templates/templatemo_546_sixteen_clothing/assets/images/team_02.jpg"
+                        alt="" />
+                    <h4>Karry Pitcher</h4>
+                    <span>Product Expert</span>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores praesentium blanditiis cumque
+                        impedit ipsam doloremque dolorum, velit quod illo fugit!</p>
+                </div>
+                <div className="image">
+                    <img src="https://templatemo.com/templates/templatemo_546_sixteen_clothing/assets/images/team_03.jpg"
+                        alt="" />
+                    <h4>Michael Soft</h4>
+                    <span>Chief Marketing</span>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta amet dignissimos pariatur at,
+                        impedit architecto ab ad enim totam assumenda.</p>
+                </div>
+                <div className="image">
+                    <img src="https://templatemo.com/templates/templatemo_546_sixteen_clothing/assets/images/team_04.jpg"
+                        alt="" />
+                    <h4>Mary Cool</h4>
+                    <span>Product Specialist</span>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt consequatur sit nostrum fugiat eius
+                        voluptatem odit saepe nobis! Quis, temporibus.</p>
+                </div>
+                <div className="image">
+                    <img src="https://templatemo.com/templates/templatemo_546_sixteen_clothing/assets/images/team_05.jpg"
+                        alt="" />
+                    <h4>George Walker</h4>
+                    <span>Product Photographer</span>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate explicabo doloremque distinctio
+                        dolorem repellat velit vitae aspernatur fuga mollitia adipisci?</p>
+                </div>
+                <div className="image">
+                    <img src="https://templatemo.com/templates/templatemo_546_sixteen_clothing/assets/images/team_06.jpg"
+                        alt="" />
+                    <h4>Kate Town</h4>
+                    <span>General Manager</span>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque aspernatur porro amet iusto dolor
+                        alias tenetur temporibus sed quas modi.</p>
+                </div>
+            </div>
+            <div className="services">
+                <div className="row">
+                    <div className="profile">
+                        <div className="icon"></div>
+                        <h4>Product Manager</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit veritatis est sint. Aliquam eius
+                            iure facere quasi voluptatibus minus veritatis!</p>
+                        <button>Read More</button>
+                    </div>
+                    <div className="profile">.
+                        <div className="icon"></div>
+                        <h4>Customer Relations</h4>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus voluptas pariatur, autem
+                            nihil quis eius voluptate provident in sequi possimus?</p>
+                        <button>Read More</button>
+                    </div>
+                    <div className="profile">
+                        <div className="icon"></div>
+                        <h4>Global Collection</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ea sit aliquid cupiditate moles</p>
+                        <button>Read More</button>
+                    </div>
+                </div>
+            </div>
+            <div className="partners">
+                <h2>Happy Partners</h2>'
+                <hr />
+            </div>
+            <div className="company">
+                <div className="item">
+                    <img src="https://templatemo.com/templates/templatemo_546_sixteen_clothing/assets/images/client-01.png" alt=" " />
+                </div>
+                <div className="item">
+                    <img src="https://templatemo.com/templates/templatemo_546_sixteen_clothing/assets/images/client-01.png" alt="" />
+                </div>
+                <div className="item">
+                    <img src="https://templatemo.com/templates/templatemo_546_sixteen_clothing/assets/images/client-01.png" alt="" />
+                </div>
+                <div className="item">
+                    <img src="https://templatemo.com/templates/templatemo_546_sixteen_clothing/assets/images/client-01.png" alt="" />
+                </div>
+                <div className="item">
+                    <img src="https://templatemo.com/templates/templatemo_546_sixteen_clothing/assets/images/client-01.png" alt="" />
+                </div>
+            </div>
+            <div className="footer">
+                <p>Copyright © 2020 Sixteen Clothing Co., Ltd.
+                </p>
+            </div>
+        </div>
 
-        <h1>This is my About page</h1>
+
+
     )
-
 }
-
 export default About;
