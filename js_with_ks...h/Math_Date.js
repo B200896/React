ac9d791fa12d9939151a32d7c28d32115270225d
mohitsh7979 <mohitsh7979@gmail.com>
2024-04-12 04:47:38 +0530

    Update

---
+++ js_with_ks...h/Math_Date.js
@@ -0,0 +1,18 @@
+// let a = 86;
+// let a = 86.78429;
+// let a = 7.158
+// let a = -87;
+
+// console.log(Math.sqrt(a))
+// console.log(Math.floor(a))
+// console.log(Math.ceil(a))
+// console.log(Math.abs(a))
+// console.log(Math.floor(Math.random()*255))
+
+
+// let date = new Date();
+// console.log(date)
+// console.log(date.getDate())
+// console.log(date.getFullYear())
+// console.log(date.getHours())
+// console.log(date.getSeconds())
---
+++ js_with_ks...h/String.js
@@ -0,0 +1,85 @@
+// in java Script string is primitive data type
+
+// let name = "mohit";
+// console.log(name)
+// console.log(typeof(name))
+
+// let name = new String("mohit")
+// console.log(name)
+// console.log(typeof(name))
+
+// let name = "Mohit";
+// let city = "Jaipur"
+
+// console.log(`My Name is ${name} and i am from ${city}`)  /* backtiks  (template litterls)*/
+
+
+// let FullName = "";
+// console.log(FullName.length)
+// console.log(FullName[1])
+
+// for(let i = 0 ; i<FullName.length ; i=i+2){
+//     console.log(FullName[i])
+// }
+
+// for(let i of FullName){
+//     console.log(i)
+// }
+
+// let FullName = "KashishKaramchandani";
+// let c = 0;
+// for(let i of FullName){
+//     c++;
+// }
+// console.log(c)
+// console.log(FullName.length)
+// console.log(FullName.indexOf("m"))
+// console.log(FullName.charAt(3))
+// console.log(FullName.charCodeAt(4))
+// console.log(FullName.substring(2,7))
+// console.log(FullName.slice(-14,2))
+// console.log(FullName.trim())
+// console.log(FullName.trimEnd())
+// console.log(FullName.toUpperCase())
+// console.log(FullName.replace("a","L"))
+// console.log(FullName.includes("u"))
+// console.log(FullName.replaceAll("a","L"))
+
+// let a = "5";
+// console.log(a.padStart(6,"-"))
+// console.log(a.padEnd(6,"-"))
+
+// let x = 1000;
+// console.log(typeof x,x)
+// let y = x.toString();
+// console.log(typeof y,y)
+// console.log(x+y)
+
+// let m = "rohit";
+// console.log(m.repeat(10))
+
+// let FullName = "kashishkaramchandani";
+// console.log(FullName.split(''))
+
+
+// let first = "kashish";
+// let last = "karamchandani";
+
+// console.log(first.concat(last))
+
+
+// let a = "I am from jaipur"
+// console.log(a.search("j"))
+
+
+
+// How many types of loops in java script 
+// what is difference between do-while loop and while loop 
+// what is difference between while loop and for loop 
+// what is difference between for loop and for of loops 
+// why we need loop and what is the advantage of loop
+
+
+
+
+
