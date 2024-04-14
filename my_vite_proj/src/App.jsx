9d7ee0565cc575edc005d0212f468737c7c70fe3
mohitsh7979 <mohitsh7979@gmail.com>
2024-04-14 21:14:42 +0530

    Uodate

---
+++ my_vite_proj/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+// import Header from "./Header";
+// import Footer from "./Footer";
+
+
+
+function App({n,c}) {
+
+  console.log(n,c)
+
+  return (
+    <>
+     <h1>My name is  and i am from  </h1>
+    </>
+  )
+}
+
+export default App;
---
+++ new.js
@@ -0,0 +1,11 @@
+// What is react and why we need react
+// What is real dom and virtual dom 
+// what is difference between real dom and virtual dom
+// what is jsx in react and why we need jsx
+// what is props and why we need props in react 
+// why we need functional component in react 
+
+// craete react app 
+// file structure
+// components
+
